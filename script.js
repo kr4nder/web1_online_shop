159af3b394dd1ee99cfb731afb1ece60b7250a49
kr4nder <den-andronov@inbox.ru>
2025-09-26 23:40:10 +0300

    add js script - reading els

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cartItemsContainer = document.getElementById('cart-items');
+    const totalEl = document.getElementById('total');
+    const buttons = document.querySelectorAll('.add-to-cart');
+
+    let cart = [];
+})
