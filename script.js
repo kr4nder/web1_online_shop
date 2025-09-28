0c580d7d08e799954e0a10afbfc464643d7d845e
kr4nder <den-andronov@inbox.ru>
2025-09-28 18:44:24 +0300

    button add to cart working

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cartItemsContainer = document.getElementById('cart-items');
     const totalEl = document.getElementById('total');
-    const buttons = document.querySelectorAll('.add-to-cart');
+    const buttons = document.querySelectorAll('.add-to-cart'); // все кнопки добавить в корзину
 
     let cart = [];
+
+
+    buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const product = button.closest("article"); // находим карточку
+        const title = product.querySelector(".product-name").textContent;
+        const price = product.querySelector(".product-price").textContent;
+        alert(("Добавлен товар: " + title + " " + price));
+    });
+    });
 })
-
-
-const buttons = document.querySelectorAll(".add-to-cart"); // все кнопки добавить в корзину
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const product = button.closest("article"); // находим карточку
-    const title = product.querySelector(".product-name").textContent;
-    const price = product.querySelector(".product-price").textContent;
-    alert("Добавлен товар:", title);
-  });
-});
