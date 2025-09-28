09009f178351773051aa697067c4bb9f653e3521
kr4nder <den-andronov@inbox.ru>
2025-09-28 19:44:41 +0300

    adding els to cart and count total

--- script.js
+++ script.js
@@ -5,13 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let cart = [];
 
+    function renderCart() { // рендер корзины
+    cartItemsContainer.innerHTML = ""; // очищаем корзину перед отрисовкой
+
+    if (cart.length === 0) {
+        cartItemsContainer.innerHTML = "<p>Корзина пуста</p>";
+        totalEl.textContent = "0";
+        return;
+    }
+
+    let total = 0;
+    const list = document.createElement("ul");
+
+    cart.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = `${item.title} — ${item.price}`;
+        list.appendChild(li);
+        total += item.price;
+    });
+
+    cartItemsContainer.appendChild(list);
+    totalEl.textContent = total;
+    }
+
 
     buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const product = button.closest("article"); // находим карточку
         const title = product.querySelector(".product-name").textContent;
-        const price = product.querySelector(".product-price").textContent;
+        const price =  Number(product.querySelector('.product-price').textContent.replace(/\D/g, ''));
         alert(("Добавлен товар: " + title + " " + price));
+
+        // добавляем товар в массив корзины
+        cart.push({ title, price });
+
+        // обновляем корзину
+        renderCart();
     });
     });
 })
