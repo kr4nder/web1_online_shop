87e60b8bfe5c4369207729884732011c426631c0
kr4nder <den-andronov@inbox.ru>
2025-12-13 01:52:35 +0300

    added favicon

--- lab3/script.js
+++ lab3/script.js
@@ -5,6 +5,15 @@ const recordsList = document.getElementById('records');
 const newGameBtn = document.getElementById('newGameBtn');
 const undoBtn = document.getElementById('undoBtn');
 
+// фавикон
+(function addFavicon() {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/png';
+  link.href = './favicon.png';
+  document.head.appendChild(link);
+})();
+
 // динамическое создание сетки
 function createGrid() {
   for (let i = 0; i < 16; i++) {
